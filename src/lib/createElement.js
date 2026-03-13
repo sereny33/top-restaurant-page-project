c44b96ab877e5f1efd6ab1bcb61848c63b13595e
Sergei Nosov <serejke33@gmail.com>
2026-03-13 13:06:48 +0300

    Add JSDoc to lib functions

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,4 +1,13 @@
-function newElement(elementType, elementClasses, elementTextContent) {
+/**
+ *  * Creates an element with given arguments.
+ * 
+ * @param {string} elementType The HTML tag name to create (e.g. "div", "button", "h2").
+ * @param {string[]} elementClasses elementClasses - Array of CSS classes to add to the element.
+ * @param {string} elementTextContent - Text content to insert inside the element.
+ * @returns {HTMLElement} The created DOM element.
+ */
+
+export function newElement(elementType, elementClasses, elementTextContent) {
   const element = document.createElement(`${elementType}`);
   elementClasses.forEach(className => {
     element.classList.add(className);
@@ -6,5 +15,3 @@ function newElement(elementType, elementClasses, elementTextContent) {
   element.textContent = elementTextContent;
   return element
 }
-
-export { newElement }
--- src/lib/createSection.js
+++ src/lib/createSection.js
@@ -1,3 +1,12 @@
+/**
+ * Creates a section element, fills it with dynamically generated elements
+ * and appends it to the main content container.
+ *
+ * @param {string} sectionName - CSS class name for the section.
+ * @param {Function[]} sectionElements - Array of functions that return DOM elements.
+ * Each function will be executed and its returned element will be appended to the section.
+
+ */
 export function createSection(sectionName, sectionElements) {
   const section = document.createElement('section');
   section.classList.add(`${sectionName}`);
